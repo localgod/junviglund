86960be296a077426b6d0edff14156475facdef8
Johannes Skov Frandsen <jsf@greenoak.dk>
2021-07-07 10:35:14 +0000

    Basic Cloudflare workser setup

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,9 +4,9 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'junviglund',
+    title: 'Junviglund',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'da'
     },
     meta: [
       { charset: 'utf-8' },
