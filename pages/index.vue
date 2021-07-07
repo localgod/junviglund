74e093946d4d2d19e63f5a0f7540e35a09b2d50d
Johannes Skov Frandsen <jsf@greenoak.dk>
2021-07-07 11:42:56 +0000

    small layout change

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,30 @@
 <template>
-  <h1><NuxtLogo /> Junviglund</h1>
+  <b-container class="main">
+    <b-row>
+      <b-col />
+      <b-col>
+        <h1><NuxtLogo /> Junviglund</h1>
+        </b-col>
+      <b-col />
+    </b-row>
+  </b-container>
 </template>
 
 <script>
 export default {}
 </script>
+
+<style>
+
+.main {
+  background-color: azure;
+  opacity: 0.9;
+  text-align: center;
+}
+
+body {
+
+ background: url(http://source.unsplash.com/MMJx78V7xS8/1600x900);
+ background-size: cover;
+}
+</style>
