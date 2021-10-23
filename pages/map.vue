48a32f60ce4b48db6cec79a8c45bc26cc1f0012b
Johannes Skov Frandsen <jsf@greenoak.dk>
2021-10-23 10:28:55 +0000

    much improved map support

--- pages/map.vue
+++ pages/map.vue
@@ -1,21 +1,27 @@
 <template>
   <b-container>
     <div id="map-wrap" style="height: 100vh">
-      <no-ssr>
+      <client-only>
         <l-map :zoom="17" :center="[55.4097702, 11.8698327]">
           <l-ruler :options="rulerOptions" />
           <l-control position="topright">
-            <b-btn @click="showFeature('estate')">Vis matrikel</b-btn>
-            <b-btn @click="hideFeature('estate')">Skjul matikel</b-btn>
-            <b-btn @click="showFeature('barn')">Vis lade</b-btn>
-            <b-btn @click="hideFeature('barn')">Skjul lade</b-btn>
+            <b-button-group>
+            <template v-for="feature in features">
+              <b-btn
+              size="sm"
+                v-bind:key="Object.keys(feature)[0]"
+                @click="toggleFeature(Object.keys(feature)[0])"
+                >{{ Object.keys(feature)[0] }}</b-btn
+              >
+            </template>
+            </b-button-group>
           </l-control>
           <l-control-layers position="topright"></l-control-layers>
 
           <l-tile-layer
             name="Sattelit"
             url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.jpg"
-            visible="false"
+            :visible="false"
             layer-type="base"
             attribution="World Imagery"
           ></l-tile-layer>
@@ -23,48 +29,95 @@
           <l-tile-layer
             name="Kort"
             url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-            visible="false"
+            :visible="true"
             layer-type="base"
             attribution="http://osm.org/copyright"
           ></l-tile-layer>
-
-          <l-geo-json v-if="estate" :geojson="estate" />
-          <l-geo-json v-if="barn" :geojson="barn" />
+          <template v-for="feature in features">
+            <l-geo-json
+              v-bind:key="feature"
+              v-if="feature[Object.keys(feature)[0]].data"
+              v-bind:geojson="feature[Object.keys(feature)[0]].data"
+            />
+          </template>
         </l-map>
-      </no-ssr>
+      </client-only>
     </div>
   </b-container>
 </template>
 
 <script>
-import LRuler from "vue2-leaflet-ruler";
+import LRuler from 'vue2-leaflet-ruler'
 export default {
-    components: {
-    "l-ruler": LRuler,
+  components: {
+    'l-ruler': LRuler,
   },
   data() {
     return {
       rulerOptions: {},
       data: undefined,
-      estate: { type: 'FeatureCollection', features: [] },
-      barn: { type: 'FeatureCollection', features: [] },
+      features: [],
     }
   },
   mounted() {
-    this.getGeoJson()
+    this.getGeoJson().then(() => {
+      this.getFeatures()
+    })
   },
   methods: {
     async getGeoJson() {
       this.data = await this.$content('geo').fetch()
     },
-    showFeature(name) {
-      const e = this.data.features.filter(item => {
-        return item.properties.name === name
+    getFeatures() {
+      const features = []
+      this.data.features.forEach((item) => {
+        const feature = {}
+        feature[item.properties.name] = { data: null }
+
+        features.push(feature)
       })
-      this[name] = e
+      this.features = features
+    },
+    toggleFeature(name) {
+      const found = this.features.findIndex((element) => {
+        if (Object.keys(element)[0] === name) {
+          return true
+        }
+        return false
+      })
+      if (found !== -1) {
+        if (this.features[found][name].data === null) {
+          this.showFeature(name)
+        } else {
+          this.hideFeature(name)
+        }
+      }
+    },
+    showFeature(name) {
+      const e = this.data.features.filter((item) => {
+        return item.properties.name === name
+      })[0]
+
+      const found = this.features.findIndex((element) => {
+        if (Object.keys(element)[0] === name) {
+          return true
+        }
+        return false
+      })
+      if (found !== -1) {
+        this.features[found][name].data = e
+      }
     },
     hideFeature(name) {
-      this[name] = undefined
+      const found = this.features.findIndex((element) => {
+        if (Object.keys(element)[0] === name) {
+          return true
+        }
+        return false
+      })
+      if (found !== -1) {
+        this.features[found][name].data = null
+      }
     },
   },
 }
