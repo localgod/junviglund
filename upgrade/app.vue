a409c7e00c6dcb41166594588537b6dcbe45906d
Johannes Skov Frandsen <jsf@greenoak.dk>
2024-01-26 13:13:09 +0000

    Added modal support for images

--- upgrade/app.vue
+++ upgrade/app.vue
@@ -1,5 +1,6 @@
 <template>
   <NuxtLayout>
+    <Navigation/>
     <NuxtPage />
   </NuxtLayout>
 </template>
--- upgrade/components/card.vue
+++ upgrade/components/card.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="card">
-        
-        <img v-if="post?.mainImage" :src="builder.image(post.mainImage).width(1200).url()" class="" alt="...">
-        
+
+        <img v-if="post?.mainImage" :src="builder.image(post.mainImage).width(1200).fit('crop').url()" class="" alt="...">
+
         <div class="card-body">
 
             <h5 class="card-title">{{ post?.title }}</h5>
@@ -11,8 +11,23 @@
             </p>
             <div v-if="post?.images" class="row row-cols-1 row-cols-md-6 g-4">
                 <div v-for="img in post.images" class="col">
-                    <div class="card">                 
-                        <img :src="builder.image(img).width(190).height(190).fit('clip').url()" class="card-img-top" alt="...">
+                    <div class="card">
+                        <img :src="builder.image(img).width(190).height(190).fit('clip').url()" class="card-img-top"
+                            alt="..." data-bs-toggle="modal" :data-bs-target="'#' + img._key">
+                    </div>
+                    <div class="modal modal-xl fade" :id="img._key" tabindex="-1" aria-labelledby="Picture"
+                        aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <img :src="builder.image(img).width(1100).fit('scale').url()">
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -30,9 +45,6 @@ const builder = imageUrlBuilder({
     projectId: runtimeConfig.public.sanityProjectId,
     dataset: runtimeConfig.public.sanityDataset,
 })
-
-
-
 defineProps<{
     post?: Post
 }>()
