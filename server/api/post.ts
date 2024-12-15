62f102e5f8cc287760b11ab603abbf75b64ce996
Johannes Skov Frandsen <jsf@greenoak.dk>
2024-12-15 18:00:24 +0000

    cors changes

--- server/api/post.ts
+++ server/api/post.ts
@@ -21,11 +21,8 @@ class CMS {
 
 export default defineEventHandler(async (event) => {
   const headers = {
-    'Access-Control-Allow-Origin': 'Same-Origin',
-    'crossOriginResourcePolicy': 'same-origin',
-    'crossOriginOpenerPolicy': 'same-origin',
-    'crossOriginEmbedderPolicy': 'require-corp',
-    'contentSecurityPolicy': "default-src 'self';base-uri 'self';font-src 'self' https: data:;form-action 'self';frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src 'self';script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests",
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Max-Age': '86400',
     'X-XSS-Protection': 1
   }
   setHeaders(event, headers)
